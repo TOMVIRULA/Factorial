b85a13ed29149fe44bbb92f7a2932dd4103c7e16
Byron Virula <byron.virula@galileo.edu>
2018-02-11 20:37:36 -0600

    Mi primer suvida.

---
+++ Factorial.java
@@ -0,0 +1,11 @@
+class Factorial{
+	public estatic void main(String args[]){
+		int i,fact=1;
+		int number=5;
+		for(i=1;i<=number;i++){
+			fact=fact*i;
+		}
+		System.out.prontln("factorial of"+number+"is:"+fact);
+	}
+	.
+}
